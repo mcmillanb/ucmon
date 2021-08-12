2cd73854988b788fac6e286c04787f542cddbc1c
Billy <billy@mcmillanworld.co.uk>
2021-08-12 16:39:51 +0100

    get variables and first run of influxdb

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@ if [ -f ".env" ]; then
 echo "File exists"
 else
 echo "File does not exist"
+touch .env
+echo "Enter username"
+read user
+echo "Enter Password (minimum 8 cahracters)"
+read password
+echo "Organisation?"
+read org
 fi
+docker run -d -p 8086:8086 -v $PWD/influxdb2/data:/var/lib/influxdb2 -v $PWD/influxdb2/config:/etc/influxdb2 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=$user -e DOCKER_INFLUXDB_INIT_PASSWORD=$password -e DOCKER_INFLUXDB_INIT_ORG=$org -e DOCKER_INFLUXDB_INIT_BUCKET=ucmon influxdb:2.0.7-alpine
