6766d995367e14d1e715dc4090f5df91454d638b
Billy <billy@mcmillanworld.co.uk>
2021-08-13 11:55:39 +0100

    variable

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ echo "DOCKER_INFLUXDB_INIT_PASSWORD=$password" >> .env
 echo "Organisation?"
 read org
 echo "DOCKER_INFLUXDB_INIT_ORG=$org" >> .env
-echo "DOCKER_INFLUXDB_INIT_MODE=setup" >> .env
+echo "DOCKER_INFLUXDB_INIT_BUCKET=ucmon" >> .env
 echo "INFLUXDB_PORT=8086" >> .env
 #docker run -d -p 8086:8086 --network ucmon --name=influxdb -v $PWD/influxdb2/data:/var/lib/influxdb2 -v $PWD/influxdb2/config:/etc/influxdb2 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=$user -e DOCKER_INFLUXDB_INIT_PASSWORD=$password -e DOCKER_INFLUXDB_INIT_ORG=$org -e DOCKER_INFLUXDB_INIT_BUCKET=ucmon influxdb:2.0.7-alpine
 fi
