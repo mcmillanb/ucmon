fc20f4a536f6f719d31be20a9a38c03226767cdd
Billy <billy@mcmillanworld.co.uk>
2021-08-13 14:05:18 +0100

    add telegraf

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,20 @@ echo "DOCKER_INFLUXDB_INIT_PASSWORD=$password" >> .env
 echo "Organisation?"
 read org
 echo "DOCKER_INFLUXDB_INIT_ORG=$org" >> .env
-echo "DOCKER_INFLUXDB_INIT_BUCKET=ucmon" >> .env
+echo "DOCKER_INFLUXDB_INIT_BUCKET=telegraf" >> .env
 echo "INFLUXDB_PORT=8086" >> .env
-echo "/nInitial build - please wait"
+echo "Initial build - please wait"
 docker-compose up -d
-echo "Done"
-tester_one=$(docker exec -it influxdb influx auth list)
-[[ $tester_one =~ $pat ]]
-echo "${BASH_REMATCH[1]}"
+pause 15
+echo "InfluxDB runnning, getting keys"
+influx_op=$(docker exec -it influxdb influx auth list)
+testVar=$(echo $influx_op | sed -e 's/\r//g')
+token=$(echo $testVar | grep -o -P '(?<=s Token ).*(?= admin)')
+echo "Token is $token"
+echo "INFLUX_TOKEN=$token" >> .env
+echo "INFLUX_HOST=http://influxdb:8086" >> .env
+echo "INFLUX_ORG=$org" >> .env
+
 
 
 #docker run -d -p 8086:8086 --network ucmon --name=influxdb -v $PWD/influxdb2/data:/var/lib/influxdb2 -v $PWD/influxdb2/config:/etc/influxdb2 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=$user -e DOCKER_INFLUXDB_INIT_PASSWORD=$password -e DOCKER_INFLUXDB_INIT_ORG=$org -e DOCKER_INFLUXDB_INIT_BUCKET=ucmon influxdb:2.0.7-alpine
