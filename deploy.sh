482e195d49fe000c5ae79e600743dd1fb56a85c4
Billy <billy@mcmillanworld.co.uk>
2021-08-13 16:17:46 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,18 @@
 if [ -f ".env" ]; then
 echo "File exists"
 else
-echo "File does not exist"
-touch .env
+echo "File does not exist"touch .env
 echo "DOCKER_INFLUXDB_INIT_MODE=setup" >> .env
-echo "Enter username"
-read user
-echo "DOCKER_INFLUXDB_INIT_USERNAME=$user" >> .env
+echo "Setting up admin user"
+#read user
+echo "DOCKER_INFLUXDB_INIT_USERNAME=admin" >> .env
 echo "Enter Password (minimum 8 characters)"
 read password
 echo "DOCKER_INFLUXDB_INIT_PASSWORD=$password" >> .env
 echo "Organisation?"
 read org
 echo "DOCKER_INFLUXDB_INIT_ORG=$org" >> .env
-echo "DOCKER_INFLUXDB_INIT_BUCKET=telegraf" >> .env
+echo "DOCKER_INFLUXDB_INIT_BUCKET=ucmon" >> .env
 echo "INFLUXDB_PORT=8086" >> .env
 echo "Initial build - please wait"
 docker-compose up -d
@@ -31,8 +30,4 @@ echo "INFLUX_TOKEN=$token" >> .env
 echo "INFLUX_HOST=http://influxdb:8086" >> .env
 echo "INFLUX_ORG=$org" >> .env
 
-
-
-
-#docker run -d -p 8086:8086 --network ucmon --name=influxdb -v $PWD/influxdb2/data:/var/lib/influxdb2 -v $PWD/influxdb2/config:/etc/influxdb2 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=$user -e DOCKER_INFLUXDB_INIT_PASSWORD=$password -e DOCKER_INFLUXDB_INIT_ORG=$org -e DOCKER_INFLUXDB_INIT_BUCKET=ucmon influxdb:2.0.7-alpine
 fi
