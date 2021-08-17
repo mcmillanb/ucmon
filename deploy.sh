fe80c151e89eee5e4f759d7e3e2b720297b26182
Billy <billy@mcmillanworld.co.uk>
2021-08-17 16:36:30 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -14,22 +14,28 @@ echo "DOCKER_INFLUXDB_INIT_PASSWORD=$password" >> .env
 echo "Organisation?"
 read org
 echo "DOCKER_INFLUXDB_INIT_ORG=$org" >> .env
-echo "DOCKER_INFLUXDB_INIT_BUCKET=ucmon" >> .env
+echo "DOCKER_INFLUXDB_INIT_BUCKET=telegraf" >> .env
 echo "INFLUXDB_PORT=8086" >> .env
 echo "Initial build - please wait"
+cp docker-compose.yml.1 docker-compose.yml
 docker-compose up -d
-echo "InfluxDB runnning, getting keys"
+echo "Waiting 30 seconds to get API key...."
 sleep 30
 influx_op=$(docker exec -it influxdb influx auth list)
 testVar=$(echo $influx_op | sed -e 's/\r//g')
 token=$(echo $testVar | grep -o -P "(?<=s Token ).*(?= $user)")
 echo "Token is $token"
+echo "Setting up initial parameters"
 echo "INFLUX_TOKEN=$token" >> .env
-echo "INFLUX_HOST=http://influxdb:8086" >> .env
+echo "INFLUX_HOST=influxdb" >> .env
 echo "INFLUX_ORG=$org" >> .env
+docker exec -it influxdb influx bucket create --name ucmon
+docker exec -it influxdb influx telegrafs create --name telegraf -f /etc/influxdb2/telegraf.conf
 
 echo "Restarting services"
 docker-compose down
+rm docker-compose.yml
+cp docker-compose.yml.2 docker-compose.yml
 docker-compose up -d
 
 fi
